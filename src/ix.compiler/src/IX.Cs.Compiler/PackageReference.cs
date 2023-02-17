14afe9f6044fada5116ca32760411fc33d77acfe
PTKu <61538034+PTKu@users.noreply.github.com>
2023-02-17 10:59:44 +0100

    wip

--- src/ix.compiler/src/IX.Cs.Compiler/PackageReference.cs
+++ src/ix.compiler/src/IX.Cs.Compiler/PackageReference.cs
@@ -26,18 +26,21 @@ public class PackageReference : IPackageReference
     /// <param name="packageReferenceNode">Package reference node from csproj file.</param>
     /// <param name="projectFile">Csproj file.</param>
     /// <exception cref="FailedToRetrievePackageReferenceException"></exception>
-    public PackageReference(XElement packageReferenceNode, string projectFile)
+    public static PackageReference CreateFromReferenceNode(XElement packageReferenceNode, string projectFile)
     {
+        string include = string.Empty;
         try
         {
-            Include = packageReferenceNode.Attribute(XName.Get(nameof(Include)))?.Value ?? packageReferenceNode.Attribute(XName.Get("Update"))?.Value;
-            Version = packageReferenceNode.Attribute(XName.Get(nameof(Version))).Value;
-            ReferencePath = PackageReferenceNugetPath(this);
+            include = packageReferenceNode.Attribute(XName.Get(nameof(Include)))?.Value ?? packageReferenceNode.Attribute(XName.Get("Update"))?.Value;
+            var version = packageReferenceNode.Attribute(XName.Get(nameof(Version))).Value;
+            var referencePath = PackageReferenceNugetPath(include, version);
+
+            return new PackageReference(referencePath, include, version);
         }
         catch (Exception ex)
         {
             throw new FailedToRetrievePackageReferenceException(
-                $"Could not parse 'Name' or 'Version' of the package '{Include}' reference in project {projectFile}",
+                $"Could not parse 'Name' or 'Version' of the package '{include}' reference in project {projectFile}",
                 ex);
         }
     }
@@ -46,11 +49,17 @@ public class PackageReference : IPackageReference
     ///     Create new instance of <see cref="PackageReference" /> from <see cref="PackageDependency" />
     /// </summary>
     /// <param name="packageDependency">Package dependency</param>
-    public PackageReference(PackageDependency packageDependency)
+    public PackageReference(PackageDependency packageDependency) 
+        : this(PackageReferenceNugetPath(packageDependency.Id, packageDependency.VersionRange.OriginalString), packageDependency.Id, packageDependency.VersionRange.OriginalString)
     {
-        Include = packageDependency.Id;
-        Version = packageDependency.VersionRange.OriginalString;
-        ReferencePath = PackageReferenceNugetPath(this);
+        
+    }
+
+    public PackageReference(string packageFolder, string id, string version)
+    {
+        Include = id;
+        Version = version;
+        ReferencePath = packageFolder;
     }
 
     /// <summary>
@@ -73,8 +82,17 @@ public class PackageReference : IPackageReference
     /// </summary>
     public string ReferencePath { get; }
 
-    private static string PackageReferenceNugetPath(PackageReference package)
+    private static string PackageReferenceNugetPath(string packageName, string packageVersion)
     {
-        return Path.Combine(NugetDir, package.Name, CsProject.GetBestMatchedVersion(package));
+        return Path.Combine(NugetDir, packageName, CsProject.GetBestMatchedVersion(packageName, packageVersion));
     }
+
+    /// <inheritdoc />
+    public string MetadataPath => Path.Combine(ReferencePath, "content", ".meta", "meta.json");
+
+    /// <inheritdoc />
+    public string ProjectInfo => Path.Combine(ReferencePath, "content", ".meta", "sourceinfo.json");
+
+    /// <inheritdoc />
+    public bool IsIxDependency => File.Exists(MetadataPath);
 }
---
+++ src/ix.compiler/tests/Ix.Compiler.CsTests/PackageReferenceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Core;
+
+namespace Ix.Compiler.CsTests
+{
+    public class PackageReferenceTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var packageFolder = "";
+            var packageReference = new PackageReference(packageFolder, "ix.package", "0.0.0");
+        }
+    }
+}
