f74746d62b7a2227ae2bc5f7a1716e98b908e7bc
Peter <61538034+PTKu@users.noreply.github.com>
2023-02-20 09:55:46 +0100

    [webapiconn] Adds segemtation of bulk requestion that exceed 64KB

--- src/ix.connectors/tests/Ix.Connector.Sax.WebAPITests/Exploratory.cs
+++ src/ix.connectors/tests/Ix.Connector.Sax.WebAPITests/Exploratory.cs
@@ -45,7 +45,7 @@ namespace Ix.Connector.S71500.WebAPITests.Exploratory
             var myBOOL = new WebApiBool(connector, "", "myBOOL");
             var myBYTE = new WebApiByte(connector, "", "myBYTE");
             var myWORD = new WebApiWord(connector, "", "myWORD");
-            //    myDWORD	        :       DWORD	      ;
+            var myDWORD = new WebApiDWord(connector, "", "myDWORD");
             var myLWORD = new WebApiLWord(connector, "", "myLWORD");
             var mySINT = new WebApiSInt(connector, "", "mySINT");
             var myINT = new WebApiInt(connector, "", "myINT");
@@ -60,69 +60,202 @@ namespace Ix.Connector.S71500.WebAPITests.Exploratory
             var myTIME = new WebApiTime(connector, "", "myTIME");
             var myLTIME = new WebApiLTime(connector, "", "myLTIME");
             var myDATE = new WebApiDate(connector, "", "myDATE");
-            //   myLDATE	        :       LDATE	      ;
+            var myLDATE = new WebApiDate(connector, "", "myLDATE");
             var myTIME_OF_DAY = new WebApiTimeOfDay(connector, "", "myTIME_OF_DAY");
-            //   myLTIME_OF_DAY	:       LTIME_OF_DAY  ;
+            var myLTIME_OF_DAY = new WebApiLTimeOfDay(connector, "", "myLTIME_OF_DAY");
             var myDATE_AND_TIME = new WebApiDateTime(connector, "", "myDATE_AND_TIME");
-            //   myLDATE_AND_TIME :      LDATE_AND_TIME;
-            //   myCHAR	        :       CHAR	      ;
-            //   myWCHAR	        :       WCHAR	      ;
+            var myLDATE_AND_TIME = new WebApiLDateTime(connector, "", "myLDATE_AND_TIME");
+            var myCHAR = new WebApiChar(connector, "", "myCHAR");
+            var myWCHAR = new WebApiWChar(connector, "", "myWCHAR");
             var mySTRING = new WebApiString(connector, "", "mySTRING");
-            //   myWSTRING	    :       WSTRING	      ;
+            var myWSTRING = new WebApiWString(connector, "", "myWSTRING");
 
-
-            List<ITwinPrimitive> primitives = new List<ITwinPrimitive> 
-            {
-                myBOOL ,
-                myBYTE ,
-                myWORD ,
-          //      DWORD  ,
-                myLWORD,
-                mySINT ,
-                myINT,
-                myDINT ,
-                myLINT ,
-                myUSINT,
-                myUINT ,
-                myUDINT,
-                myULINT,
-                myREAL ,
-                myLREAL,
-                myTIME ,
-                myLTIME,
-                myDATE ,
-                //   myLDATE	        :
-                myTIME_OF_DAY,
-                //   myLTIME_OF_DAY	:
-                myDATE_AND_TIME,
-                //   myLDATE_AND_TIME
-                //   myCHAR	        :
-                //   myWCHAR	     
-                mySTRING,
-                //   myWSTRING	    :
-            };
-            
             List<ITwinPrimitive> huge = new List<ITwinPrimitive>();
+            
+                huge.Add(myBOOL);
+                huge.Add(myBYTE);
+                huge.Add(myWORD);
+                huge.Add(myDWORD);
+                huge.Add(myLWORD);
+                huge.Add(mySINT);
+                huge.Add(myINT);
+                huge.Add(myDINT);
+                huge.Add(myLINT);
+                huge.Add(myUSINT);
+                huge.Add(myUINT);
+                huge.Add(myUDINT);
+                huge.Add(myULINT);
+                huge.Add(myREAL);
+                huge.Add(myLREAL);
+                huge.Add(myTIME);
+                huge.Add(myLTIME);
+                huge.Add(myDATE);
+                huge.Add(myLDATE);
+                huge.Add(myTIME_OF_DAY);
+                huge.Add(myLTIME_OF_DAY);
+                huge.Add(myDATE_AND_TIME);
+                huge.Add(myLDATE_AND_TIME);
+                huge.Add(myCHAR);
+                huge.Add(myWCHAR);
+                huge.Add(mySTRING);
+                huge.Add(myWSTRING);
+            
 
-            for (int i = 0; i < 29; i++)
-            {
-                huge.AddRange(primitives);
+            for (int i = 0; i < 1000; i++)
+            {                                                                                              
+                huge.Add(new WebApiBool(connector, "", "myBOOL")                 );
+                huge.Add(new WebApiByte(connector, "", "myBYTE")                 );
+                huge.Add(new WebApiWord(connector, "", "myWORD")                 );
+                huge.Add(new WebApiDWord(connector, "", "myDWORD")               );
+                huge.Add(new WebApiLWord(connector, "", "myLWORD")               );
+                huge.Add(new WebApiSInt(connector, "", "mySINT")                 );
+                huge.Add(new WebApiInt(connector, "", "myINT")                   );
+                huge.Add(new WebApiDInt(connector, "", "myDINT")                 );
+                huge.Add(new WebApiLInt(connector, "", "myLINT")                 );
+                huge.Add(new WebApiUSInt(connector, "", "myUSINT")               );
+                huge.Add(new WebApiUInt(connector, "", "myUINT")                 );
+                huge.Add(new WebApiUdInt(connector, "", "myUDINT")               );
+                huge.Add(new WebApiULInt(connector, "", "myULINT")               );
+                huge.Add(new WebApiReal(connector, "", "myREAL")                 );
+                huge.Add(new WebApiLReal(connector, "", "myLREAL")               );
+                huge.Add(new WebApiTime(connector, "", "myTIME")                 );
+                huge.Add(new WebApiLTime(connector, "", "myLTIME")               );
+                huge.Add(new WebApiDate(connector, "", "myDATE")                 );
+                huge.Add(new WebApiDate(connector, "", "myLDATE")                );
+                huge.Add(new WebApiTimeOfDay(connector, "", "myTIME_OF_DAY")     );
+                huge.Add(new WebApiLTimeOfDay(connector, "", "myLTIME_OF_DAY")   );
+                huge.Add(new WebApiDateTime(connector, "", "myDATE_AND_TIME")    );
+                huge.Add(new WebApiLDateTime(connector, "", "myLDATE_AND_TIME")  );
+                huge.Add(new WebApiChar(connector, "", "myCHAR")                 );
+                huge.Add(new WebApiWChar(connector, "", "myWCHAR")               );
+                huge.Add(new WebApiString(connector, "", "mySTRING")             );
+                huge.Add(new WebApiWString(connector, "", "myWSTRING"));
             }
 
-            //for (int i = 0; i < 1; i++)
-            {
-                huge.AddRange(primitives.Take(11));
-            }
 
-           
-
+            
             await connector.ReadBatchAsync(huge);
-           
-           
-
+            
             Assert.Equal(42, myBYTE.Cyclic);
             Assert.True(myBOOL.Cyclic);
             Assert.Equal(43, myINT.Cyclic);
+        }
+
+        [Fact]
+        public async void should_write_huge()
+        {
+#if RELEASE
+    return;
+#endif
+            var connector = new WebApiConnector(TargetIp, "Everybody", "", true);
+
+            await connector.WriteAsync<byte>("myBYTE", 42);
+            await connector.WriteAsync<bool>("myBOOL", true);
+            await connector.WriteAsync<short>("myINT", 43);
+
+            var myBOOL = new WebApiBool(connector, "", "myBOOL");
+            var myBYTE = new WebApiByte(connector, "", "myBYTE");
+            var myWORD = new WebApiWord(connector, "", "myWORD");
+            var myDWORD = new WebApiDWord(connector, "", "myDWORD");
+            var myLWORD = new WebApiLWord(connector, "", "myLWORD");
+            var mySINT = new WebApiSInt(connector, "", "mySINT");
+            var myINT = new WebApiInt(connector, "", "myINT");
+            var myDINT = new WebApiDInt(connector, "", "myDINT");
+            var myLINT = new WebApiLInt(connector, "", "myLINT");
+            var myUSINT = new WebApiUSInt(connector, "", "myUSINT");
+            var myUINT = new WebApiUInt(connector, "", "myUINT");
+            var myUDINT = new WebApiUdInt(connector, "", "myUDINT");
+            var myULINT = new WebApiULInt(connector, "", "myULINT");
+            var myREAL = new WebApiReal(connector, "", "myREAL");
+            var myLREAL = new WebApiLReal(connector, "", "myLREAL");
+            var myTIME = new WebApiTime(connector, "", "myTIME");
+            var myLTIME = new WebApiLTime(connector, "", "myLTIME");
+            var myDATE = new WebApiDate(connector, "", "myDATE");
+            var myLDATE = new WebApiDate(connector, "", "myLDATE");
+            var myTIME_OF_DAY = new WebApiTimeOfDay(connector, "", "myTIME_OF_DAY");
+            var myLTIME_OF_DAY = new WebApiLTimeOfDay(connector, "", "myLTIME_OF_DAY");
+            var myDATE_AND_TIME = new WebApiDateTime(connector, "", "myDATE_AND_TIME");
+            var myLDATE_AND_TIME = new WebApiLDateTime(connector, "", "myLDATE_AND_TIME");
+            var myCHAR = new WebApiChar(connector, "", "myCHAR");
+            var myWCHAR = new WebApiWChar(connector, "", "myWCHAR");
+            var mySTRING = new WebApiString(connector, "", "mySTRING");
+            var myWSTRING = new WebApiWString(connector, "", "myWSTRING");
+
+            List<ITwinPrimitive> huge = new List<ITwinPrimitive>();
+
+           
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                huge.Add(new WebApiBool(connector, "", "myBOOL"));
+                huge.Add(new WebApiByte(connector, "", "myBYTE"));
+                huge.Add(new WebApiWord(connector, "", "myWORD"));
+                huge.Add(new WebApiDWord(connector, "", "myDWORD"));
+                huge.Add(new WebApiLWord(connector, "", "myLWORD"));
+                huge.Add(new WebApiSInt(connector, "", "mySINT"));
+                huge.Add(new WebApiInt(connector, "", "myINT"));
+                huge.Add(new WebApiDInt(connector, "", "myDINT"));
+                huge.Add(new WebApiLInt(connector, "", "myLINT"));
+                huge.Add(new WebApiUSInt(connector, "", "myUSINT"));
+                huge.Add(new WebApiUInt(connector, "", "myUINT"));
+                huge.Add(new WebApiUdInt(connector, "", "myUDINT"));
+                huge.Add(new WebApiULInt(connector, "", "myULINT"));
+                huge.Add(new WebApiReal(connector, "", "myREAL"));
+                huge.Add(new WebApiLReal(connector, "", "myLREAL"));
+                huge.Add(new WebApiTime(connector, "", "myTIME"));
+                huge.Add(new WebApiLTime(connector, "", "myLTIME"));
+                huge.Add(new WebApiDate(connector, "", "myDATE"));
+                huge.Add(new WebApiDate(connector, "", "myLDATE"));
+                huge.Add(new WebApiTimeOfDay(connector, "", "myTIME_OF_DAY"));
+                huge.Add(new WebApiLTimeOfDay(connector, "", "myLTIME_OF_DAY"));
+                huge.Add(new WebApiDateTime(connector, "", "myDATE_AND_TIME"));
+                huge.Add(new WebApiLDateTime(connector, "", "myLDATE_AND_TIME"));
+                huge.Add(new WebApiChar(connector, "", "myCHAR"));
+                huge.Add(new WebApiWChar(connector, "", "myWCHAR"));
+                huge.Add(new WebApiString(connector, "", "mySTRING"));
+                huge.Add(new WebApiWString(connector, "", "myWSTRING"));
+            }
+
+            huge.Add(myBOOL);
+            huge.Add(myBYTE);
+            huge.Add(myWORD);
+            huge.Add(myDWORD);
+            huge.Add(myLWORD);
+            huge.Add(mySINT);
+            huge.Add(myINT);
+            huge.Add(myDINT);
+            huge.Add(myLINT);
+            huge.Add(myUSINT);
+            huge.Add(myUINT);
+            huge.Add(myUDINT);
+            huge.Add(myULINT);
+            huge.Add(myREAL);
+            huge.Add(myLREAL);
+            huge.Add(myTIME);
+            huge.Add(myLTIME);
+            huge.Add(myDATE);
+            huge.Add(myLDATE);
+            huge.Add(myTIME_OF_DAY);
+            huge.Add(myLTIME_OF_DAY);
+            huge.Add(myDATE_AND_TIME);
+            huge.Add(myLDATE_AND_TIME);
+            huge.Add(myCHAR);
+            huge.Add(myWCHAR);
+            huge.Add(mySTRING);
+            huge.Add(myWSTRING);
+
+            myBYTE.Cyclic =  48;
+            myBOOL.Cyclic = true;
+            myINT.Cyclic = 55;
+
+            await connector.WriteBatchAsync(huge);
+
+            await connector.ReadBatchAsync(huge);
+
+            Assert.Equal(48, myBYTE.Cyclic);
+            Assert.True(myBOOL.Cyclic);
+            Assert.Equal(55, myINT.Cyclic);
         }
     }
 }
