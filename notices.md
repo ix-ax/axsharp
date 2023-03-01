08af612012016ef05b99e7577273623f0a45145a
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2023-03-01 16:00:34 +0100

    [NEW-FEATURE] Implement string localization and interpolation (#99)
    * Create draft PR for #98
    
    * Create project for resx compiler
    
    * added fundamentals for string localization and some basic testing
    
    * added wraper with location of localized string to dictionary
    
    * abstract legalacrobatics into ix.compiler project
    
    * Create ResxManager
    
    * added support for attributeName pragmas, added support for multiple localized strings
    
    * Change options for launchSettings, small refactoring
    
    * Added test for CreateIdentifier and GetOnlyLocalizedString
    
    * Added more supported symbols, change to relative path in comments
    
    * adds tests to L1,2,3
    
    ---------
    
    Co-authored-by: Brano5 <Brano5@users.noreply.github.com>
    Co-authored-by: Branko Zachemsky <bzachemsky@gmail.com>
    Co-authored-by: Dávid Špavor <xspavo00@vutbr.cz>

--- notices.md
+++ notices.md
@@ -370,3 +370,17 @@ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PA
 AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# ResXResourceReader.NetStandard
+
+https://github.com/farlee2121/ResXResourceReader.NetStandard/blob/main/LICENSE.md
+
+MIT License
+
+Copyright (c) 2020 Spencer Farley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/ix.compiler/src/ixr/LocalizedStringWrapper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ix.ixr_doc
+{
+    public class LocalizedStringWrapper
+    {
+        // composite object with location and raw value
+        public Dictionary<string, StringValueWrapper> LocalizedStringsDictionary {get; private set; }
+        private Regex _localizedStringRegex;
+        private Regex _attributeNameRegex;
+        public LocalizedStringWrapper()
+        {
+            LocalizedStringsDictionary = new Dictionary<string, StringValueWrapper>();
+            _localizedStringRegex = new Regex("<#(.*?)#>",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+             _attributeNameRegex = new Regex("#ix-set:AttributeName",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+
+        }
+
+        public string CreateId(string rawText)
+        { 
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in rawText)
+            {
+                //try to find character in mappings
+                var mapchar = SymbolMappings.TryToGetValueByChar(c);
+                if(mapchar != null)
+                { 
+                    sb.Append(mapchar); 
+                }
+                //add only if is letter
+                else if (Char.IsLetter(c))
+                {
+                    sb.Append(c); 
+                }
+                else
+                {
+                     //unknown
+                    sb.Append('_'); 
+                }
+            }
+            return sb.ToString();
+    
+          
+        }
+
+        public bool IsValidId(string id) 
+        {
+            return Microsoft.CodeAnalysis.CSharp.SyntaxFacts.IsValidIdentifier(id);
+        }
+
+        public IEnumerable<string> TryToGetLocalizedStrings(string text)
+        { 
+            //match only text within <# #>
+            var matches = _localizedStringRegex.Matches(text).ToList();
+            if(matches.Count > 0) 
+            {
+                return matches.Select(m => m.Value);;
+            }
+        
+            return null; 
+        
+        }
+        public string GetRawTextFromLocalizedString(string text)
+        {
+           // deletes <# from beginning and #> from the end
+           return text.Substring(2,text.Length-4);
+    
+        }
+
+        public bool IsAttributeNamePragmaToken(string text)
+        {
+            return _attributeNameRegex.IsMatch(text);
+        }
+    }
+    
+
+    public class StringValueWrapper
+    {
+        public StringValueWrapper(string rawValue, string fileName, int line)
+        {
+            RawValue = rawValue;
+            FileName = fileName;
+            Line = line;
+        }
+
+        public string RawValue { get; set; }
+        public string FileName { get; set; }
+        public int Line {get; set;}
+    }
+}
