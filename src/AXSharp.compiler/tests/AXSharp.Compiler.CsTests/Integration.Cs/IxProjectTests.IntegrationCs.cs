e94e59bc7f75c4630e873fe8d74060af29d9d6ef
Peter <61538034+PTKu@users.noreply.github.com>
2023-03-22 11:52:53 +0100

    mend

--- src/AXSharp.compiler/tests/AXSharp.Compiler.CsTests/Integration.Cs/IxProjectTests.IntegrationCs.cs
+++ src/AXSharp.compiler/tests/AXSharp.Compiler.CsTests/Integration.Cs/IxProjectTests.IntegrationCs.cs
@@ -1,11 +1,3 @@
-// AXSharp.Compiler.CsTests
-// Copyright (c) 2023 Peter Kurhajec (PTKu), MTS,  and Contributors. All Rights Reserved.
-// Contributors: https://github.com/ix-ax/ix/graphs/contributors
-// See the LICENSE file in the repository root for more information.
-// https://github.com/ix-ax/ix/blob/master/LICENSE
-// Third party licenses: https://github.com/ix-ax/ix/blob/master/notices.md
-
-using Xunit;
 // AXSharp.CompilerTests
 // Copyright (c)2022 Peter Kurhajec and Contributors All Rights Reserved.
 // Contributors: https://github.com/ix-ax/ix/graphs/contributors
