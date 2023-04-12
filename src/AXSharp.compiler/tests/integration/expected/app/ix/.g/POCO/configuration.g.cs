7a73ddd7c015d4043ee0aa45030e584718e5f85e
Peter <61538034+PTKu@users.noreply.github.com>
2023-04-12 14:20:54 +0200

    asp fixing test issue from previous commit

--- src/AXSharp.compiler/tests/integration/expected/app/ix/.g/POCO/configuration.g.cs
+++ src/AXSharp.compiler/tests/integration/expected/app/ix/.g/POCO/configuration.g.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Pocos
 {
-    public partial class app
+    public partial class appTwinController
     {
         public lib1.MyClass lib1_MyClass { get; set; } = new lib1.MyClass();
         public lib2.MyClass lib2_MyClass { get; set; } = new lib2.MyClass();
