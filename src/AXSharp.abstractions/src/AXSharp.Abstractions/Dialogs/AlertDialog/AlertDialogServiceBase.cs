6c66d56b88a5dc2b54b972ff06c4f9009ea03e74
Branko Zachemsky <bzachemsky@gmail.com>
2023-06-15 12:28:11 +0200

    AlertDialogServiceBase comment Exception

--- src/AXSharp.abstractions/src/AXSharp.Abstractions/Dialogs/AlertDialog/AlertDialogServiceBase.cs
+++ src/AXSharp.abstractions/src/AXSharp.Abstractions/Dialogs/AlertDialog/AlertDialogServiceBase.cs
@@ -10,34 +10,30 @@ namespace AXSharp.Abstractions.Dialogs.AlertDialog
     {
         public event EventHandler AlertDialogChanged;
 
-        public void AddToast(string type, string title, string message, int time)
-        {
-            throw new NotImplementedException();
-        }
-
         public void AddAlertDialog(string type, string title, string message, int time)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void AddAlertDialog(IAlertDialog toast)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public List<IAlertDialog> GetAlertDialogs()
         {
-            throw new NotImplementedException();
+            return new List<IAlertDialog>();
+            //throw new NotImplementedException();
         }
 
         public void RemoveAlertDialog(IAlertDialog toast)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void RemoveAllAlertDialogs()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
