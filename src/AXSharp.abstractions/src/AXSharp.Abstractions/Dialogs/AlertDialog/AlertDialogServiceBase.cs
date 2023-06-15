aca2280b29246d988b0f5afa9ef8ab682430838c
Brano5 <97288158+Brano5@users.noreply.github.com>
2023-06-15 08:47:08 +0200

    Dialogs implementations (#188)
    * WIP - initial Dialogs Implementations
    
    * Working on AlertDialogs
    
    ---------
    
    Co-authored-by: Peter Kurhajec <61538034+PTKu@users.noreply.github.com>

---
+++ src/AXSharp.abstractions/src/AXSharp.Abstractions/Dialogs/AlertDialog/AlertDialogServiceBase.cs
@@ -0,0 +1,43 @@
+﻿using AXSharp.Abstractions.Dialogs.AlertDialog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace AXSharp.Abstractions.Dialogs.AlertDialog
+{
+    public class AlertDialogServiceBase : IAlertDialogService
+    {
+        public event EventHandler AlertDialogChanged;
+
+        public void AddToast(string type, string title, string message, int time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAlertDialog(string type, string title, string message, int time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAlertDialog(IAlertDialog toast)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IAlertDialog> GetAlertDialogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAlertDialog(IAlertDialog toast)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAllAlertDialogs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
