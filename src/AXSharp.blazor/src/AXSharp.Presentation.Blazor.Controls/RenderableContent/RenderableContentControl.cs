747e53fe2674ae636427e26d071e049ffea07d08
Brano5 <97288158+Brano5@users.noreply.github.com>
2023-07-24 13:08:27 +0200

    Clean Up from Dialogues (#196)

--- src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/RenderableContent/RenderableContentControl.cs
+++ src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/RenderableContent/RenderableContentControl.cs
@@ -20,7 +20,6 @@ using AXSharp.Presentation.Blazor.Exceptions;
 using System.Reflection;
 using System.ComponentModel;
 using AXSharp.Connector.ValueTypes;
-using AXSharp.Abstractions.Dialogs.AlertDialog;
 
 namespace AXSharp.Presentation.Blazor.Controls.RenderableContent
 {
@@ -84,20 +83,6 @@ namespace AXSharp.Presentation.Blazor.Controls.RenderableContent
         private NavigationManager _navigationManager { get; set; }
         private Type _groupContainer { get; set; }
         public Type MainLayoutType { get; set; }
-
-
-        private IAlertDialogService _alertDialogService;
-
-        [Parameter]
-        public IAlertDialogService AlertDialogService {
-            get
-            {
-                if(_alertDialogService == null)
-                    _alertDialogService = new AlertDialogServiceBase();
-                return _alertDialogService;
-            }
-            set => _alertDialogService = value;
-        }
 
         private ITwinElement _context { get; set; }
         protected override void OnInitialized()
