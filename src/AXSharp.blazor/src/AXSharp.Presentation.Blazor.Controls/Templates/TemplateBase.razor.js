c78cd439422b471d8ecb01bed5eed69942fc9c94
Brano5 <97288158+Brano5@users.noreply.github.com>
2024-03-19 06:46:42 +0100

    Remove addToolTips javascript call from TemplateBase (#295)

--- src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/Templates/TemplateBase.razor.js
+++
@@ -1,4 +0,0 @@
-﻿export function addToolTips() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-}
